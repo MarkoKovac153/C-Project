358c35009047be93693579621885fc45d78a316d
Marko Kovac <markokovac153@gmail.com>
2024-07-22 15:37:09 +0100

    rendering a quad, moved main window functionality to render_init.c. added files to buil bat and finished off other .h,.c files for initial commits.

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
-set files=src\glad.c src\main.c
+set files=src\glad.c src\main.c src\engine\global.c src\engine\render\render.c src\engine\render\render_init.c
 set libs=C:\Users\marko\Desktop\personalStuff\Github\cproject\lib\SDL2main.lib C:\Users\marko\Desktop\personalStuff\Github\cproject\lib\SDL2.lib C:\Users\marko\Desktop\personalStuff\Github\cproject\lib\freetype.lib
 
 CL /ZI /I C:\Users\marko\Desktop\personalStuff\Github\cproject\include %files% /link %libs% /OUT:mygame.exe
