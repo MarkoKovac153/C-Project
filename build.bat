b70e5ab10c133acbedd7adf566c5b9e6844c0d29
Marko Kovac <109769153+MarkoKovac153@users.noreply.github.com>
2024-07-31 17:22:16 +0100

    added shaders

--- build.bat
+++ build.bat
@@ -1,4 +1,6 @@
-set files=src\glad.c src\main.c src\engine\global.c src\engine\render\render.c src\engine\render\render_init.c
-set libs=C:\Users\marko\Desktop\personalStuff\Github\cproject\lib\SDL2main.lib C:\Users\marko\Desktop\personalStuff\Github\cproject\lib\SDL2.lib C:\Users\marko\Desktop\personalStuff\Github\cproject\lib\freetype.lib
+set render=src\engine\render\render.c src\engine\render\render_init.c src\engine\render\render_util.c 
+set io=src\engine\io\io.c 
+set files=src\glad.c src\main.c src\engine\global.c %render% %io%
+set libs=C:\Users\marko\Desktop\Projects\C-Project\lib\SDL2main.lib C:\Users\marko\Desktop\Projects\C-Project\lib\SDL2.lib C:\Users\marko\Desktop\Projects\C-Project\lib\freetype.lib
 
-CL /ZI /I C:\Users\marko\Desktop\personalStuff\Github\cproject\include %files% /link %libs% /OUT:mygame.exe
+CL /ZI /I C:\Users\marko\Desktop\Projects\C-Project\include %files% /link %libs% /OUT:mygame.exe
