aff67c94873760dcff31dc4a707ef0d6414ed1ac
Marko Kovac <109769153+MarkoKovac153@users.noreply.github.com>
2024-08-07 10:36:23 +0100

    revamp, added libs

--- src/engine/util.h
+++
@@ -1,9 +0,0 @@
-#ifndef UTIL_H
-#define UTIL_H
-
-#include <stdio.h>
-
-#define ERROR_EXIT(...) {fprintf(stderr, __VA_ARGS__); exit(1); }
-#define ERROR_RETURN(R, ...) {fprintf(stderr, __VA_ARGS__); return R; }
-
-#endif
